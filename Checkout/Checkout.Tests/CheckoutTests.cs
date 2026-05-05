0387eb79d37d8a0115431d74ffce921576a41995
ThomasGrant <thomasgrant0801@gmail.com>
2026-05-05 21:43:53 +0100

    remove init test and add in basic checkout test structure

---
+++ Checkout/Checkout.Tests/CheckoutTests.cs
@@ -0,0 +1,42 @@
+﻿using Checkout.Core;
+namespace Checkout.Tests
+{
+    public class CheckutTests
+    {
+        [Test]
+        [TestCase(new[] { "A" }, 50)]
+        public void Test_Scan_Items_And_Get_Total_Price(string[] skus, int expectedPrice)
+        {
+            var checkout = CreateCheckout();
+
+            foreach (var sku in skus)
+            {
+                checkout.Scan(sku);
+            }
+
+            var totalPrice = checkout.GetTotalPrice();
+
+            Assert.That(totalPrice, Is.EqualTo(expectedPrice));
+        }
+
+
+        private ICheckout CreateCheckout()
+        {
+            var products = new Dictionary<string, int>
+            {
+                { "A", 50 },
+                { "B", 30 },
+                { "C", 20 },
+                { "D", 15 }
+            };
+
+            var offers = new Dictionary<string, OfferRule>
+            {
+                { "A", new OfferRule("A", 3, 130) },
+                { "B", new OfferRule("B", 2, 45) }
+            };
+
+            return new Core.Checkout(products, offers);
+        }
+    }
+}
