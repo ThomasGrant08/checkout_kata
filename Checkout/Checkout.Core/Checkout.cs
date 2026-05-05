d8db09fbf790f7c59910e682826cbdf0379a4bd2
ThomasGrant <thomasgrant0801@gmail.com>
2026-05-05 21:28:14 +0100

    add total price calculation

--- Checkout/Checkout.Core/Checkout.cs
+++ Checkout/Checkout.Core/Checkout.cs
@@ -1,14 +1,32 @@
 ﻿namespace Checkout.Core
 {
-    public class Checkout : ICheckout
+    public class Checkout(Dictionary<string, int> products, Dictionary<string, OfferRule> offerRules) : ICheckout
     {
         private readonly List<string> _items = new();
+        private readonly Dictionary<string, int> _products = products;
+        private readonly Dictionary<string, OfferRule> _offerRules = offerRules;
 
         public void Scan(string sku)
         {
             _items.Add(sku);
         }
 
-        public int GetTotalPrice() => 0;
+        public int GetTotalPrice()
+        {
+            var totalPrice = 0;
+            var itemCounts = _items.GroupBy(sku => sku).ToDictionary(group => group.Key, group => group.Count());
+            foreach (var itemCount in itemCounts)
+            {
+                if (_offerRules.TryGetValue(itemCount.Key, out var offerRule))
+                {
+                    totalPrice += offerRule.CalculatePrice(itemCount.Value);
+                }
+                else
+                {
+                    totalPrice += itemCount.Value * _products[itemCount.Key];
+                }
+            }
+            return totalPrice;
+        }
     }
 }
