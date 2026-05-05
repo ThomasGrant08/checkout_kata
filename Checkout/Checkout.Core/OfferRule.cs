1bf659b7051513c327be16fa9afafbc28c442874
ThomasGrant <thomasgrant0801@gmail.com>
2026-05-05 21:18:38 +0100

    add in offer price class

---
+++ Checkout/Checkout.Core/OfferRule.cs
@@ -0,0 +1,17 @@
+﻿namespace Checkout.Core
+{
+    public class OfferRule
+    {
+        public string Sku { get; set; }
+        public int UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public int OfferPrice { get; set; }
+        public OfferRule(string sku, int unitPrice, int quantity, int offerPrice)
+        {
+            Sku = sku;
+            UnitPrice = unitPrice;
+            Quantity = quantity;
+            OfferPrice = offerPrice;
+        }
+    }
+}
