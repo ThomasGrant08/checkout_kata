f7985331d192907da6bc610eb47ce0d219b154f2
ThomasGrant <thomasgrant0801@gmail.com>
2026-05-05 21:43:27 +0100

    change offer rule to be simpler

--- Checkout/Checkout.Core/OfferRule.cs
+++ Checkout/Checkout.Core/OfferRule.cs
@@ -3,22 +3,21 @@
     public class OfferRule
     {
         public string Sku { get; set; }
-        public int UnitPrice { get; set; }
         public int Quantity { get; set; }
         public int OfferPrice { get; set; }
-        public OfferRule(string sku, int unitPrice, int quantity, int offerPrice)
+        public OfferRule(string sku, int quantity, int offerPrice)
         {
             Sku = sku;
-            UnitPrice = unitPrice;
             Quantity = quantity;
             OfferPrice = offerPrice;
         }
 
-        public int CalculatePrice(int quantity)
+        public int CalculatePrice(int itemCount, int unitPrice)
         {
-            int offerCount = quantity / Quantity;
-            int remainingCount = quantity % Quantity;
-            return offerCount * OfferPrice + remainingCount * UnitPrice;
+            var offerGroups = itemCount / Quantity;
+            var remainder = itemCount % Quantity;
+
+            return (offerGroups * OfferPrice) + (remainder * unitPrice);
         }
     }
 }
