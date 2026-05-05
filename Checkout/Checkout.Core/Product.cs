996031947474144a2a94dfb0749216855504f2bd
ThomasGrant <thomasgrant0801@gmail.com>
2026-05-05 21:12:24 +0100

    add basic product class

---
+++ Checkout/Checkout.Core/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Checkout.Core
+{
+    public class Product
+    {
+        public string Sku { get; set; }
+        public int Price { get; set; }
+
+        public Product(string sku, int price)
+        {
+            Sku = sku;
+            Price = price;
+        }
+    }
+}
